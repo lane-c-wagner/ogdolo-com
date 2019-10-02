335d97c16a6515d97c07a810cd8915f77b4dcde0
lane-c-wagner <lane.c.wagner@gmail.com>
2019-10-02 06:33:09 -0600

    website done

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+rm -r public
+rm -r resources
+hugo
 aws s3 sync --acl public-read --delete public/ s3://ogdolo.com --profile ogdolo
 aws cloudfront create-invalidation --distribution-id E1M9AZ4VGJC1LC --paths '/*' --profile ogdolo
