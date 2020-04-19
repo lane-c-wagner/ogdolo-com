5d62d0fd43c9fab69dd469f2476340a2caa40add
lane-c-wagner <lane.c.wagner@gmail.com>
2020-04-19 11:42:08 -0600

    github pages

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -r public
-rm -r resources
-hugo --minify
-aws s3 sync --acl public-read --delete public/ s3://ogdolo.com --profile ogdolo
-aws cloudfront create-invalidation --distribution-id E1M9AZ4VGJC1LC --paths '/*' --profile ogdolo
